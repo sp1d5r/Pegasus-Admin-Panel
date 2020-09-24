f133a572b8f89e1ad39eda78ce223bb27c8eea0b
sp1d5r <aa1719@imperial.ac.uk>
2020-09-24 19:57:00 +0100

    added static.json

--- server/server.js
+++ server/server.js
@@ -1,8 +1,12 @@
-const express = require('express');
 const path = require('path');
+const express = require('express');
 const app = express();
-app.use(express.static(path.join(__dirname, 'build')));
-app.get('/', function(req, res) {
-    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+const publicPath = path.join(__dirname, '..', 'public');
+const port = process.env.PORT || 3000;
+app.use(express.static(publicPath));
+app.get('*', (req, res) => {
+    res.sendFile(path.join(publicPath, 'index.html'));
 });
-app.listen(9000);
+app.listen(port, () => {
+    console.log('Server is up!');
+});
