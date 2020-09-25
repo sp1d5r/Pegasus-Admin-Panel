67fb4794d8909add719b3c4969123e3c8f015a9d
sp1d5r <aa1719@imperial.ac.uk>
2020-09-25 11:50:20 +0100

    .env file added for local development

--- src/firebase.js
+++ src/firebase.js
@@ -1,15 +1,15 @@
 import * as firebase from 'firebase';
 
-const firebaseConfig = {
-    apiKey: process.env.API_KEY,
-    authDomain: process.env.AUTH_DOMAIN,
-    databaseURL: process.env.DATABASE_URL,
-    projectId: process.env.PROJECT_ID,
-    storageBucket: process.env.STORAGE_BUCKET,
-    messagingSenderId: process.env.MESSAAGING_SENDER_ID,
-    appId: process.env.APP_ID,
-    measurementId: process.env.MEASUREMENT_ID
+const config = {
+    apiKey: process.env.REACT_APP_API_KEY,
+    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+    databaseURL: process.env.REACT_APP_DATABASE_URL,
+    projectId: process.env.REACT_APP_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
 };
+
+const firebaseConfig = config;
 
 firebase.initializeApp(firebaseConfig);
 
